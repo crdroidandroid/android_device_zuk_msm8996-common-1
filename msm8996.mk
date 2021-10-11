ed211c5dfd35365af05bf55654cacc6b5c1ac1bd
dmd79 <davidevinavil@gmail.com>
2021-10-11 21:58:19 +0200

    msm8996-common: Set PRODUCT_BROKEN_VERIFY_USES_LIBRARIES

--- msm8996.mk
+++ msm8996.mk
@@ -31,6 +31,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
