a3241dfd8f7d1fc0282b9e754717f650e0139ffe
dmd79 <davidevinavil@gmail.com>
2021-10-11 21:45:59 +0200

    Revert "msm8996-common: Import qti power HAL and rename it to android.hardware.power-service-zuk"
    This reverts commit b5d0756f2a569e7a83ad6b48c31333126588a969.

--- msm8996.mk
+++ msm8996.mk
@@ -335,9 +335,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-zuk \
-    vendor.qti.hardware.perf@1.0 \
-    vendor.qti.hardware.perf@2.0
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
