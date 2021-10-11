e56266d8ce32cf384987d9ac062d7fc82aa1ec01
dmd79 <davidevinavil@gmail.com>
2021-10-11 21:44:15 +0200

    msm8996-common: Build libqti_vndfwk_detect

--- msm8996.mk
+++ msm8996.mk
@@ -347,7 +347,9 @@ PRODUCT_COPY_FILES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson
+    libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor
 
 # RenderScript
 PRODUCT_PACKAGES += \
