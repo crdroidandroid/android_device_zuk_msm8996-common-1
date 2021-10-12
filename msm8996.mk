ef5198ef37bbda1be05d9fa3be4f09e8b6b87366
dmd79 <davidevinavil@gmail.com>
2021-10-12 17:59:53 +0200

    msm8996-common: Build vendor variant radio

--- msm8996.mk
+++ msm8996.mk
@@ -380,9 +380,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio@1.2.vendor \
+    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
     librmnetctl \
     libxml2
 
